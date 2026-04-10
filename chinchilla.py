3ffe43faa4516e6307318546fa746c376511a69c
MeinChef <126389277+MeinChef@users.noreply.github.com>
2026-04-10 18:07:07 +0200

    added crude first steps

---
+++ chinchilla.py
@@ -0,0 +1,37 @@
+from config import GPTConfiguration
+import subprocess
+import os
+
+
+if __name__ == "__main__":
+    nanoGPTpath = os.path.join(
+        os.path.dirname(__file__),
+        "nanoGPT"
+    )
+
+    config1 = GPTConfiguration(
+        n_head = 2,
+        n_layer = 2,
+        n_embed = 128,
+        name = "trials"
+    )
+
+    # this overrides the values you passed earlier, if you passed any
+    # config1.set_backend()
+    # config1.set_compile()
+
+
+    subprocess.run(
+        [
+            "python",
+            "-m",
+            "train",
+            os.path.join(
+                nanoGPTpath,
+                "config",
+                f"train_shakespeare_char_{config1.name}.py"
+            )
+        ],
+        cwd = nanoGPTpath,
+        check=True
+    )
