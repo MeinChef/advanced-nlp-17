cc4ece507f503e68540311626a6287bf880a8a02
vicktoriabehr <vicktoria.behr@gmail.com>
2026-04-28 14:21:12 +0200

    sollte jetzt klappen mit shakespear checkpoint file

--- lora_sanity_check.py
+++ lora_sanity_check.py
@@ -2,7 +2,7 @@ import torch
 from model import GPT, GPTConfig, freeze_base_params, count_trainable
 
 # Load the same checkpoint for both models
-ckpt = torch.load('out-shakespeare-5-320-1/ckpt.pt', map_location='cpu')
+ckpt = torch.load('nano-gpt/out-shakespeare-5-320-1/ckpt.pt', map_location='cpu')
 
 # Load with LoRA
 config_lora = GPTConfig(**{**ckpt['model_args'], 'lora_rank': 4})
