921fcae6e4b758c51915579aad0ce4249a612f60
MeinChef <126389277+MeinChef@users.noreply.github.com>
2026-04-29 16:37:08 +0200

    code cosmetics
    also removed dependency upon train_conf, since config.py should have everything

--- part_2_main.py
+++ part_2_main.py
@@ -1,27 +1,70 @@
 import part_2_prepare_sft
-import part_2_train_config
+# import part_2_train_config # replace with GPTConfiguration
 import part_2_training
 import part_2_evaluation
+from config import GPTConfiguration
 import shutil
 import os
+import torch
 
 
-models = ['Task 1', 'Task 2', 'Multi-Task', 'Pre-Trained']
-#preparing the data
-for model in models:
-    part_2_prepare_sft.prepare_training(model)
+if __name__ == "__main__":
+    models = ['task1', 'task2', 'multi', 'pre']
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    # preparing the data
+    for model in models:
+        part_2_prepare_sft.prepare_training(model)
 
-#prepare the training files
-part_2_train_config.prepare_configs(device_type='cuda', has_logs=false) #IF FALSE WILL CREATE LOG FILES BUT ERASE ALL PREVIOUS ONES
+    # TODO: create logs folder
+    # prepare the training files
+    # part_2_train_config.prepare_configs(
+    #     device_type = device,
+    #     has_logs = False
+    # ) 
 
-#train the models
-#COMPUTATIONALLY EXPENSIVE
-for model in models:
-    part_2_training.train(model)
-new_dest = shutil.move(os.path.join(os.path.dirname(__file__), 'nanoGPT/part_2_logs'), os.path.dirname(__file__))
+    # IF has_logs FALSE WILL CREATE LOG FILES BUT ERASE ALL PREVIOUS ONES
 
-#evaluate the models
-with open(os.path.join(os.path.dirname(__file__), 'part_2_logs/accuracies.log'), 'w') as f:
-    f.close()
-for model in models:
-    part_2_evaluation.evaluate_model(model)
+    # train the models
+    # COMPUTATIONALLY EXPENSIVE
+    for model in models:
+        cfg = GPTConfiguration(
+            n_layer = 5,
+            n_head = 5,
+            n_embed = 320,
+            lr = 1e-3 if model == "pre" else 1e-4,
+            dataset = f"shakespeare_{model if model != 'pre' else 'char'}",
+            name = model
+        )
+        cfg.set_backend()
+        cfg.write(
+            os.path.join(
+                os.path.dirname(__file__), 
+                "nanoGPT"
+            )
+        )
+
+        part_2_training.train(model)
+
+    new_dest = shutil.move(
+        os.path.join(
+            os.path.dirname(__file__), 
+            'nanoGPT',
+            'part_2_logs'
+        ), 
+        os.path.dirname(__file__)
+    )
+
+    # create file if didn't exist
+    with open(os.path.join(
+        os.path.dirname(__file__), 
+        'part_2_logs',
+        'accuracies.log'
+    ), 'a+') as f:
+        f.close()
+
+    # evaluate the models
+    for model in models:
+        part_2_evaluation.evaluate_model(
+            model = model,
+            device = device
+        )
